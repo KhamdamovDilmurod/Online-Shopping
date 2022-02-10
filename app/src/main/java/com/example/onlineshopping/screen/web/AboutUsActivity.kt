ba98aaf57c5975bb63eec3002618d53a4469d63b
KhamdamovDilmurod <hamdamovdilmurod1925@gmail.com>
2022-02-10 11:32:46 +0500

    user qismi o'zgartirildi

---
+++ app/src/main/java/com/example/onlineshopping/screen/web/AboutUsActivity.kt
@@ -0,0 +1,40 @@
+package com.example.onlineshopping.screen.web
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebViewClient
+import com.example.onlineshopping.R
+import com.example.onlineshopping.databinding.ActivityAboutUsBinding
+
+class AboutUsActivity : AppCompatActivity() {
+    lateinit var binding: ActivityAboutUsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAboutUsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // WebViewClient allows you to handle
+        // onPageFinished and override Url loading.
+        binding.webView.webViewClient = WebViewClient()
+
+        // this will load the url of the website
+        binding.webView.loadUrl("https://www.amazon.com/gp/css/homepage.html?ref_=nav_em_ya_0_1_1_35")
+
+        // this will enable the javascript settings
+        binding.webView.settings.javaScriptEnabled = true
+
+        // if you want to enable zoom feature
+        binding.webView.settings.setSupportZoom(true)
+    }
+
+    // if you press Back button this code will work
+    override fun onBackPressed() {
+        // if your webview can go back it will go back
+        if (binding.webView.canGoBack())
+            binding.webView.goBack()
+        // if your webview cannot go back
+        // it will exit the application
+        else
+            super.onBackPressed()
+    }
+
+}
